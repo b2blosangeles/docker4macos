0ec539d3fcf5a23a327ef391be27574b46ed10c6
john.xu <john.xu@trinet.com>
2020-05-16 23:26:58 -0700

    update

---
+++ _localChannel/admin/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+const port = 10000;
+var env = {
+    root : __dirname,
+    uiTrinetLocalFolder : path.join(__dirname, '..')
+}
+
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies   
+  extended: true
+})); 
+
+app.get(/(.+)$/i, (req, res) => {
+    res.send('hello w');
+    return true;
+    try {
+        delete require.cache[__dirname + '/app.js'];
+        var APP = require(__dirname + '/app.js');
+        var app = new APP(env, req, res);
+        app.get();
+    } catch (err) {
+        res.send(err.toString());
+    }
+
+});
+
+app.post(/(.+)$/i, (req, res) => {
+    try {
+        delete require.cache[__dirname + '/app.js'];
+        var APP = require(__dirname + '/app.js');
+        var app = new APP(env, req, res);
+        app.post();
+    } catch (err) {
+        res.send(err.toString());
+    }
+
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
