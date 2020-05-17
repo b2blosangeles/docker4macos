6dacc94d664a39a925ed70020c157aee3b78f8ff
john.xu <john.xu@trinet.com>
2020-05-17 09:21:42 -0700

    update

---
+++ setup/cronjob.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+DOCKERCMD=$1
+
+SCRIPTDIR=$(cd `dirname $0` && pwd)
+ROOTPATH="$(dirname "$SCRIPTDIR")"
+
+mkdir -p $ROOTPATH/_localChannel/tasks
+folder=$ROOTPATH/_localChannel/tasks
+mkdir -p /tmp/_localChannel
+markfile=/tmp/_localChannel/mark.data
+
+cmd="$DOCKERCMD -v &> /dev/null"
+
+# --- clean longer time task -----
+for file in $(find $markfile -not -newermt '-120 seconds') ;do
+   rm -fr $file >/dev/null 2>&1
+done
+
+for f in "$folder"/*; do
+  if [ -f "$markfile" ]; then
+    break;
+  fi
+  if [ -f "$f" ]; then
+    echo $f >  $markfile
+
+    fn=/tmp/SH_$(basename $f)_$(date +%s%N).sh
+    cmdd="cp -fr $f $fn && rm -fr $f && sh $fn $BASEDIR $DOCKERCMD && rm -fr $fn && rm -fr $markfile "
+    eval " $cmdd"
+  fi
+done
