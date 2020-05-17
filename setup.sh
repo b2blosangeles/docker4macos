6f7067ddf577d64586b5d706c20b0b208ce532cc
learnjs2020 <54968697+learnjs2020@users.noreply.github.com>
2020-05-16 19:21:48 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
-docker network rm network_ui_app
-
-docker network create \
-    --driver=bridge \
-    --subnet=10.10.10.0/16 \
-    --ip-range=10.10.10.0/24 \
-    --gateway=10.10.10.254 \
-    network_ui_app
-
-# docker stop $(ps docker -q -a)
-
-# --------- cron job ------------
-
-user=$1
 DOCKERCMD=$(command -v docker)
+OSENV=""
+case "$(uname -s)" in
 
-echo {"\"DOCKERCMD\"":"\"$DOCKERCMD\"", "\"ROOT\"": "\"$PWD\""} > $PWD/ui-trinet-local/admin/DOCKERCMD.json
+   Darwin)
+     OSENV='Mac'
+     ;;
+   Linux)
+     OSENV='Linux'
+     ;;
 
-chmod 777 /etc/hosts
+   CYGWIN*|MINGW32*|MSYS*|MINGW*)
+     OSENV='MS Windows'
+     ;;
+   *)
+     OSENV='' 
+     ;;
+esac
 
-sed '/echo _UI_APP/d' /var/at/tabs/$user > /tmp/crontab_$user
-cp -f /tmp/crontab_$user /var/at/tabs/$user
 
-echo "@reboot echo _UI_APP && sh $PWD/cronStart.sh $DOCKERCMD >> /tmp/cronjob_$user.log" >> /var/at/tabs/$user
 
-for (( i=1; i < 60; i+=1 ))
-do
-    echo "* * * * *  (sleep $i ; echo _UI_APP && sh $PWD/cronjob.sh $DOCKERCMD >> /tmp/cronjob_$user.log)" >> /var/at/tabs/$user
- done
+if [ $USER != $SUDO_USER ] && [ $USER == "root" ] ;
+then
+    if [ $OSENV != "Mac" ]; then
+        echo "Error : We only support Mac OS X now!"
+        exit 0
+    fi
+    echo {"\"DOCKERCMD\"":"\"$DOCKERCMD\"", "\"ROOT\"": "\"$PWD\"", "\"OSENV\"": "\"$OSENV\""} > $PWD/_localChannel/admin/DOCKERCMD.json
+    
+    sed '/echo _UI_APP/d' /var/at/tabs/$SUDO_USER  > /tmp/crontab_$SUDO_USER
+    cp -f /tmp/crontab_$SUDO_USER  /var/at/tabs/$SUDO_USER
+    chmod 777 /etc/hosts
+
+    echo "@reboot echo _UI_APP && sh $PWD/cronStart.sh $DOCKERCMD >> /tmp/cronjob_$SUDO_USER.log" >> /var/at/tabs/$SUDO_USER
+
+    for (( i=1; i < 60; i+=1 ))
+    do
+        echo "* * * * *  (sleep $i ; echo _UI_APP && sh $PWD/cronjob.sh $DOCKERCMD >> /tmp/cronjob_$SUDO_USER.log)" >> /var/at/tabs/$SUDO_USER
+    done
+
+else
+    echo "Error : Need sudo run the command!"
+fi
