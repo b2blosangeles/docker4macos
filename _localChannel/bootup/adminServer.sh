39575a9da4310322a47aea3faead9c268dd71600
john.xu <john.xu@trinet.com>
2020-05-17 00:04:00 -0700

    update

--- _localChannel/bootup/adminServer.sh
+++ _localChannel/bootup/adminServer.sh
@@ -9,12 +9,12 @@ echo "booting admin server at $NOW - $WORKFOLDER"
 cd $WORKFOLDER
 
 $DOCKERCMD build -f _adminDockerFiles/admin/DockerfileAdmin -t local_admin_image .
-$DOCKERCMD container stop local_admin_admin
-$DOCKERCMD container rm local_admin_admin
+$DOCKERCMD container stop local_admin_container
+$DOCKERCMD container rm local_admin_container
 
 # --restart=always
 
 $DOCKERCMD run -d --network=network_ui_app -p 10000:10000 -v "$WORKFOLDER/_localChannel":/var/_localChannel \
---name local_channel_admin  local_admin_image
+--name local_admin_container  local_admin_image
 
 echo "Finished to boot admin."
